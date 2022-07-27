16e0505038ee5fa7e7afe67038ff5a83af410e15
Si Thu Tin <sithu5897@gmail.com>
2022-07-27 22:05:37 +0630

    ui setup

--- basic-test/calculator/src/App.js
+++ basic-test/calculator/src/App.js
@@ -1,10 +1,18 @@
-
-
+import { useState } from 'react';
 function App() {
+  const [instruction,setInstruction] = useState('');
+  const [answer,setAnswer] = useState('');
   return (
     <div className="App">
-      
-    </div>
+      <input 
+        type="text"
+        value={instruction}
+        onChange={e=>setInstruction(e.target.value)}
+      />
+      <button>Submit</button>
+      <p>This is Answer!!!!!!!!!</p>
+      <p>{answer}</p>
+    </div>  
   );
 }
 
