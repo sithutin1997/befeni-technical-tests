44b4a840d19c922aee9aafdc4c0cd17e59a0e738
Si Thu Tin <sithu5897@gmail.com>
2022-07-28 10:53:42 +0630

    page setup

--- advanced-frontend-test/fabric-book/src/App.js
+++ advanced-frontend-test/fabric-book/src/App.js
@@ -1,8 +1,13 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/fabric/:id" element={<Fabric />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
