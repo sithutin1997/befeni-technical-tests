b2c064ba6c6419d0d25e62693be2811d86f0d841
Si Thu Tin <sithu5897@gmail.com>
2022-07-31 11:19:14 +0630

    restart function

--- basic-test/calculator/src/App.js
+++ basic-test/calculator/src/App.js
@@ -1,19 +1,41 @@
-import { useState } from 'react';
-import {handleAnswer} from './Helper'
+import { useState, useEffect } from "react";
+import { handleAnswer, restartCalculator } from "./Helper";
 function App() {
-  const [instruction,setInstruction] = useState('');
-  const [answer,setAnswer] = useState('');
+  const [instruction, setInstruction] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [isFinished, setIsFinished] = useState(false);
+  const [isRestart, setIsRestart] = useState(false);
+  const startOver = () => {
+    setIsRestart(true);
+    setIsFinished(false);
+    setAnswer("");
+  };
+  useEffect(() => {
+    return () => {
+      restartCalculator();
+    };
+  }, [isRestart]);
+
   return (
     <div className="App">
-      <input 
+      <input
         type="text"
         value={instruction}
-        onChange={e=>setInstruction(e.target.value)}
+        onChange={(e) => setInstruction(e.target.value)}
+        disabled={isFinished}
       />
-      <button onClick={()=>handleAnswer(instruction,setInstruction,setAnswer)}>Submit</button>
-      <p>This is Answer!!!!!!!!!</p>
-      <p>{answer}</p>
-    </div>  
+      <button
+        onClick={() =>
+          handleAnswer(instruction, setInstruction, setAnswer, setIsFinished)
+        }
+        disabled={isFinished}
+      >
+        Submit
+      </button>
+      <span style={{ marginLeft: "30px" }}>The Answer is - </span>
+      <span>{answer}</span>
+      <button onClick={() => startOver()}>Start Over</button>
+    </div>
   );
 }
 
--- basic-test/calculator/src/Helper.js
+++ basic-test/calculator/src/Helper.js
@@ -1,50 +1,59 @@
 //object of operator functions for user input
 const operatorObj = {
-    'multiply' : function(result,value) {
-        return result*value;
-    },
-    'add' : function(result,value) {
-        return result+value;
-    },
-    'subtract' : function(result,value) {
-        return result-value;
-    },
-    'divide' : function(result,value) {
-        return result/value;
-    }
-}
+  multiply: function (result, value) {
+    return result * value;
+  },
+  add: function (result, value) {
+    return result + value;
+  },
+  subtract: function (result, value) {
+    return result - value;
+  },
+  divide: function (result, value) {
+    return result / value;
+  },
+};
 let numValue = [];
 let operatorValue = [];
 let result = 0;
 
-export const handleAnswer = (instruction,setInstruction,setAnswer) => {
-    //separate the instruction string
-    let spaceIndex = instruction.indexOf(' ')
-    let operatorSubString = instruction.substring(0,spaceIndex);
-    let valueSubString = instruction.substring(spaceIndex+1);
-    let intValue = parseInt(valueSubString);
-    operatorValue.push(operatorSubString);
-    numValue.push(intValue);
-    
-    //check user input apply or not
-    if(operatorSubString === 'apply') {
-        //reverse looping for calculation
-        for(let i = numValue.length - 1; i >= 0; i--) {
-            if(operatorValue[i] === 'apply') {
-                result = numValue[i];
-            }else {
-                result = process(result,numValue[i],operatorValue[i])
-            }
-        }
-        setAnswer(result)
+export const handleAnswer = (
+  instruction,
+  setInstruction,
+  setAnswer,
+  setIsFinished
+) => {
+  //separate the instruction string
+  let spaceIndex = instruction.indexOf(" ");
+  let operatorSubString = instruction.substring(0, spaceIndex);
+  let valueSubString = instruction.substring(spaceIndex + 1);
+  let intValue = parseInt(valueSubString);
+  operatorValue.push(operatorSubString);
+  numValue.push(intValue);
+
+  //check user input apply or not
+  if (operatorSubString === "apply") {
+    //reverse looping for calculation
+    for (let i = numValue.length - 1; i >= 0; i--) {
+      if (operatorValue[i] === "apply") {
+        result = numValue[i];
+      } else {
+        result = process(result, numValue[i], operatorValue[i]);
+      }
     }
-    //clear input field
-    setInstruction('')
-    
-}
+    setAnswer(result);
+    setIsFinished(true);
+  }
+  //clear input field
+  setInstruction("");
+};
 
-
+export const restartCalculator = () => {
+  result = 0;
+  operatorValue = [];
+  numValue = [];
+};
 //calculate function for user input
-const process = (result,value,operator) => {
-    return (operatorObj[operator](result,value))
-} 
+const process = (result, value, operator) => {
+  return operatorObj[operator](result, value);
+};
